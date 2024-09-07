5aecda4ca59510e64e2c1a02554e96a1b25ace54
EmilleReis48 <emillereis48@gmail.com>
2024-09-06 22:15:06 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,39 @@
+let dados = [
+{
+    titulo : "A Muralha da China: Um Gigante Milenar",
+    descricao: "Sabia que se você pudesse esticar a Muralha da China em linha reta, ela daria quase duas voltas ao redor da Terra? Isso mesmo! Essa estrutura gigantesca, construída ao longo de séculos, se estende por milhares de quilômetros, atravessando montanhas, desertos e planícies. Uma das construções mais impressionantes já erguidas pelo homem, a Muralha da China é um verdadeiro monumento à engenhosidade e ao esforço humano.",
+    link: "https://pt.wikipedia.org/wiki/Muralha_da_China",
+    tags: "muralha china viagem"
+},
+{
+    titulo : "O Everest não é o lugar mais alto da Terra",
+    descricao : "  O Everest é a montanha mais alta do mundo, medida a partir do nível do mar. O ponto mais alto da Terra, quando medido a partir do centro da Terra é o cume do Chimborazo, no Equador. Por que essa diferença? A Terra não é uma esfera perfeita. Ela é ligeiramente achatada nos polos e mais larga no equador. Essa diferença na forma da Terra faz com que o ponto mais alto, quando medido a partir do centro da Terra, não seja necessariamente o mesmo ponto que está mais alto em relação ao nível do mar.",
+    link : "https://pt.wikipedia.org/wiki/Chimbora%C3%A7o",
+    tags: "everest alto terra montanha chimborazo"
+},
+{
+    titulo : "O maior Deserto do Mundo não tem Areia",
+    descricao : "O maior deserto do mundo em termos de área não é o Saara, mas sim o deserto antártico. Por que a Antártida é considerada um deserto? A definição de deserto não se limita apenas à presença de areia e altas temperaturas. Um deserto é caracterizado principalmente pela extremamente baixa precipitação. A Antártida, apesar de coberta por uma espessa camada de gelo, recebe uma quantidade mínima de neve ao longo do ano, o que a classifica como um deserto polar.",
+    link : "https://pt.wikipedia.org/wiki/Deserto_polar",
+    tags: "saara deserto areia polar antartica antartico gelo chuva"
+},
+{
+    titulo : "Challenge Deep, o ponto mais profundo do Oceano",
+    descricao : " A Fossa das Marianas é, o ponto mais profundo conhecido dos oceanos e, consequentemente, o lugar mais profundo da crosta terrestre. Sua profundidade supera a altura do Everest. Challenge Deep tem aproximadamente 11.034 metros, enquanto o Monte Everest, a montanha mais alta do mundo, tem cerca de 8.848 metros. Isso significa que o Everest poderia ser 'submerso' na Fossa das Marianas e ainda sobraria espaço.",
+    link : "https://pt.wikipedia.org/wiki/Fossa_das_Marianas",
+    tags: "oceano profundo mar everest crosta supera fossa"
+},
+{
+    titulo : "O Alaska não era um estado Americano",
+    descricao : " Em 1867, os Estados Unidos compraram o Alasca da Rússia por 7,2 milhões de dólares, uma quantia considerada alta na época, o que gerou muitas críticas. Após a compra, o Alasca se tornou um território não incorporado dos Estados Unidos. Isso significa que ele não fazia parte de nenhum estado, mas era administrado pelo governo federal. Em 1959, o Alasca foi oficialmente admitido como o 49º estado dos Estados Unidos. Na época da compra, não se conhecia a riqueza em recursos naturais do Alasca, como petróleo e minerais.",
+    link : "https://pt.wikipedia.org/w/index.php?search=O+Alaska+n%C3%A3o+era+um+estado+Americano++Mostrar+rascunhos&title=Especial%3APesquisar&ns0=1",
+    tags: "russia america compra preço recursos riqueza "
+},
+{
+    titulo : "O peso total das formigas no mundo, pode superar o dos humanos",
+    descricao : " É inimaginável! Por cada humano, há 2,5 milhões de formigas. Estima-se que existam cerca de 20 quatrilhões de formigas no mundo! Isso significa 20.000.000.000.000.000 de formigas! É um número tão grande que é difícil de imaginar. O peso de todas elas juntas, é estimada em cerca de 12 megatoneladas de carbono seco. Isso significa que, sebpudéssemos reunir todas as formigas do planeta em uma balança gigante, elas pesariam mais do que todas as aves e mamíferos selvagens juntos! A biomassa total das formigas é estimada em cerca de 20% da biomassa total dos humanos. Isso significa que, em termos de massa corporal, as formigas representam uma parte significativa do planeta.",
+    link : "https://pt.wikipedia.org/w/index.php?search=O+Alaska+n%C3%A3o+era+um+estado+Americano++Mostrar+rascunhos&title=Especial%3APesquisar&ns0=1",
+    tags: "peso massa carbono maior humanos mamiferos"
+},
+];
+
